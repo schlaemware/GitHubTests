04939daaf4d214050b332f56f2ca3e2a1ef8fdfc
Michael Schlaepfer <m.schlaepfer@printplus.ch>
2023-03-24 14:20:56 +0100

    New dev function

--- Solution/ConsoleTestApp/Program.cs
+++ Solution/ConsoleTestApp/Program.cs
@@ -1,14 +1,15 @@
 ﻿using Domain;
 
 namespace ConsoleTestApp {
-    internal class Program {
-        private static void Main(string[] args) {
-            Console.WriteLine("Hello, world!");
+  internal class Program {
+    private static void Main(string[] args) {
+      Console.WriteLine("Hello, world!");
 
-            ExampleService service = new();
-            int result = service.Addition(5, 3);
-
-            Console.WriteLine("5 + 3 = " + result.ToString());
-        }
+      byte[] bytes = File.ReadAllBytes(@"D:\GitHub\GitHubTests\Solution\PackagedTestApp\PackagedTestApp (Package)\PackagedTestApp (Package)_TemporaryKey.pfx");
+      using (StreamWriter writer = new(new FileStream(@"C:\Temporary\SigningCertificate_Encoded.txt", FileMode.Create))) {
+        writer.Write(System.Convert.ToBase64String(bytes));
+        writer.Flush();
+      }
     }
+  }
 }
