91f9c585f65afc22d6e1229dccfee297d5e60376
Michael Schlaepfer <m.schlaepfer@printplus.ch>
2023-03-27 08:16:59 +0200

    Program.cs

--- Solution/ConsoleTestApp/Program.cs
+++ Solution/ConsoleTestApp/Program.cs
@@ -4,12 +4,6 @@ namespace ConsoleTestApp {
   internal class Program {
     private static void Main(string[] args) {
       Console.WriteLine("Hello, world!");
-
-      byte[] bytes = File.ReadAllBytes(@"D:\GitHub\GitHubTests\Solution\PackagedTestApp\PackagedTestApp (Package)\PackagedTestApp (Package)_TemporaryKey.pfx");
-      using (StreamWriter writer = new(new FileStream(@"C:\Temporary\SigningCertificate_Encoded.txt", FileMode.Create))) {
-        writer.Write(System.Convert.ToBase64String(bytes));
-        writer.Flush();
-      }
     }
   }
 }
